fafc29d753c943200f903b8bfe94ebc667bdb724
AndriusVegan <lifecoachandy7@gmail.com>
2021-12-17 21:45:37 +0200

    fixed message layout just need to pull avatar image

--- components/Message.js
+++ components/Message.js
@@ -16,32 +16,30 @@ function Message({ message }) {
       <div className={`relative h-8 w-8 ${isUserMessage && "order-last ml-2"}`}>
         <Avatar username={message.get("username")} />
       </div>
+
       <div
-        className={`flex space-x-4 p-3 rounded-lg
-      `}
+        className={`flex space-x-4 p-3 rounded-lg ${
+          isUserMessage
+            ? "rounded-br-none bg-pink-300"
+            : "rounded-bl-non bg-blue-400"
+        }`}
       >
-        <div
-          className={`flex space-x-4 p-3 rounded-lg ${
-            isUserMessage
-              ? "rounded-br-none bg-pink-300"
-              : "rounded-bl-non bg-blue-400"
-          }`}
-        >
-          <TimeAgo
-            datetime={message.createdAt}
-            className={`text-[10px] italic text-gray-400 ${
-              isUserMessage && "order-first pr-1"
-            }`}
-          />
-          <p
-            className={`absolute bottom-5 text-xs ${
-              isUserMessage ? "text-pink-300" : "text-blue-400"
-            }`}
-          >
-            {message.get("message")}
-          </p>
-        </div>
+        <p> {message.get("message")}</p>
       </div>
+
+      <TimeAgo
+        className={`text-[10px] italic text-gray-400 ${
+          isUserMessage && "order-first pr-1"
+        }`}
+        datetime={message.createdAt}
+      />
+      <p
+        className={`absolute -bottom-5 text-xs ${
+          isUserMessage ? "text-pink-300" : "text-blue-400"
+        }`}
+      >
+        {message.get("username")}
+      </p>
     </div>
   );
 }
